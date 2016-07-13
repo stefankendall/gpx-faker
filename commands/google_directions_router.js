30e8d72e8a7c481fba22b3064af5d972ea20e538
Stefan Kendall <stefankendall@gmail.com>
2016-07-13 19:19:42 -0400

    Add loops to google directions.

--- commands/google_directions_router.js
+++ commands/google_directions_router.js
@@ -2,14 +2,18 @@ const GoogleDirectionParser = require('./../lib/google_direction_parser');
 const GpxWriter = require('./../lib/gpx_writer');
 var _ = require('lodash');
 
-var convert = function (url, speed, pauses) {
+var convert = function (url, speed, pauses, loops) {
     var segmentPoints = GoogleDirectionParser.parse(url);
     var points = [];
-    for (var i = 0; i < segmentPoints.length - 1; i++) {
-        var betweenPoints = segmentPoints[i].pointsBetween(segmentPoints[i + 1], speed);
-        points = points.concat(betweenPoints);
-        if (pauses) {
-            points = points.concat(pointsNearby(segmentPoints[i + 1], 20));
+    loops = loops || 1;
+    console.log("Creating %s segment loops", loops);
+    for (var loop = 0; loop < loops; loop++) {
+        for (var i = 0; i < segmentPoints.length - 1; i++) {
+            var betweenPoints = segmentPoints[i].pointsBetween(segmentPoints[i + 1], speed);
+            points = points.concat(betweenPoints);
+            if (pauses) {
+                points = points.concat(pointsNearby(segmentPoints[i + 1], 20));
+            }
         }
     }
     new GpxWriter(points).writeTo('./paths/out.gpx');
